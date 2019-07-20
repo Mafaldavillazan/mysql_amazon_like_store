53eba8cd409b88cfe72f68f50c13540d0c9475c6
Mafalda <mafalda.villazan@gmail.com>
2019-07-19 18:58:01 -0700

    Adding the changes on the dab when the user inputs the information

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -67,16 +67,49 @@ function selectProduct() {
             }
         ])
         .then(function (answer) {
-            var query = "SELECT product_name FROM products WHERE item_id = ?";
-            connection.query(query,[answer.id_selection], function (err, res) {
+            var query = "SELECT item_id,product_name,price,stock_quantity FROM products WHERE item_id = ?";
+            connection.query(query, [answer.id_selection], function (err, res) {
                 if (err) throw err;
                 //Display the selection
                 for (var i = 0; i < res.length; i++) {
-                    console.log("-----------------");
-                    console.log("RESULT:" + JSON.stringify(res[i],null,2));
+                    console.log("-----------------")
+                    console.log("YOU WANT TO BUY: "
+                        + res[i].product_name
+                        + " || PRIZE: $" + res[i].price);
+
+                    //If the value is less than the stock quantity say insufficient quantity
+                    if (res[i].stock_quantity < answer.quantity) {
+                        console.log("Insuficient Quantity")
+                        displayProduct();
+                    }
+                    else {
+                        console.log("We can sell toy some of it")
+                        //Input information that will run the function to update the DB
+                        updateStock(res[i].stock_quantity,answer.quantity, res[i].item_id)
+                    }
                 }
             });
         });
+}
 
+//Updating the data base with the new value
+function updateStock(resQuantity,answerQuantity, itemID) {
+    var updatedResult = answerQuantity - resQuantity
+    var query = connection.query(
+        "UPDATE products SET ? WHERE ?",
+        [
+            {
+                stock_quantity : updatedResult
+            
+            },
+            {
+                item_id: itemID
+            }
+        ],
+        function (err, res) {
+            if (err) throw err;
+            console.log(res + "Product updated!")
+        }
+    );
 
 }
